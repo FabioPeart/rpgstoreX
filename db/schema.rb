d3bd23701f0d44fa006b52d1513c36f43ccd49c5
FabioPeart <fabiopeart@gmail.com>
2018-11-04 21:44:58 -0600

    004- Added Model Usertype

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_034220) do
+ActiveRecord::Schema.define(version: 2018_11_05_034418) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 2018_11_05_034220) do
     t.decimal "pst"
     t.decimal "hst"
     t.decimal "total_rate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "usertypes", force: :cascade do |t|
+    t.string "type_description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
