9c5216f4dd99b2be0b431120fab21edcdaa6b7e1
FabioPeart <fabiopeart@gmail.com>
2018-11-04 21:43:33 -0600

    003- Runned db seed, added Model Province

---
+++ db/migrate/20181105034220_create_provinces.rb
@@ -0,0 +1,14 @@
+class CreateProvinces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :provinces do |t|
+      t.string :name
+      t.string :initials
+      t.decimal :gst
+      t.decimal :pst
+      t.decimal :hst
+      t.decimal :total_rate
+
+      t.timestamps
+    end
+  end
+end
